3fb328a035ad21c1975d82611115b7930a7982e0
sseonmo <mo3509@gmail.com>
2019-10-19 01:47:06 +0900

    [19.10.18ㄹ9][selenium_crawling] 1. 초록나라(http://ecolife.me.go.kr) 크롤링 - 완

--- com/08_ecolife.py
+++ com/08_ecolife.py
@@ -1,25 +1,52 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from time import sleep
+import json
+import pandas as pd
+import openpyxl
 
 driver = webdriver.Chrome(
 	executable_path='../webdriver/chromedriver'
 )
 
-url = 'http://ecolife.me.go.kr/ecolife/chmstryProduct/chmstryProductIndex?prdCd=01&page=6'
+def getProductInfo(tr):
+	tds = tr.findAll("td")
+	return {"no": tds[0].text, "name": tds[1].text, "category": tds[2].text,
+			"vendor": tds[3].text, "confirmNo": tds[4].text, "licenceNo": tds[5].text}
+
+def parser(pageString):
+	bsObj = BeautifulSoup(pageString, "html.parser")
+	table = bsObj.find("table", {"class": "table table-striped2"})
+	tbody = table.find("tbody")
+	trs = tbody.findAll("tr")
+	productInfos = []
+	for tr in trs:
+		productInfo = getProductInfo(tr)
+		productInfos.append(productInfo)
+	return productInfos
+
+def save(df, filename):
+    writer = pd.ExcelWriter(filename)
+    df.to_excel(writer, 'Sheet1')
+    writer.save()
+
+url = 'http://ecolife.me.go.kr/ecolife/sntryAid/index?page=6'
 driver.get(url)
 
-url = 'http://ecolife.me.go.kr/ecolife/chmstryProduct/chmstryProductIndex?prdCd=01&page=6'
-driver.get(url)
+def getInfos(pageNo):
+	url = 'http://ecolife.me.go.kr/ecolife/sntryAid/index?page={}'.format(pageNo)
+	driver.get(url)
+	pageString = driver.page_source
+	return parser(pageString)
 
-def parser(pageStr):
-	bsObj = BeautifulSoup(pageStr, 'html.parser')
+result = []
+for page in range(1, 10 + 1):
+	result = result + getInfos(page)
 
-	table = bsObj.find('table', {'class': 'table table-striped2'})
-	aEle = bsObj.select('table.table-striped2 > tbody > tr > td > a')
-	# print(type(aEle))
-	# trs = table.findAll('tr')
-	# print(trs[0])
-	return list([a.text for a in aEle])
+df = pd.DataFrame(data=result)
+print(df.count())
+print(df.head())
+save(df, "./ecolife.xlsx")
 
-pageString = driver.page_source
-print(parser(pageString))
+sleep(10)
+driver.close()
